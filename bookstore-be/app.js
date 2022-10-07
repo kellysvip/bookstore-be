4ee5e3751ee98be63b7bd3d61e5c75f6e4c3c40f
Kelly <20522051@gm.uit.edu.vn>
2022-10-08 01:59:55 +0700

    step0 - importfiles + error hdl

--- bookstore-be/app.js
+++ bookstore-be/app.js
@@ -1,10 +1,17 @@
+require(
+    "dotenv"
+).config()
+const cors= require("cors")
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
+
+
 
 var app = express();
 
@@ -13,8 +20,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+//catch when request match no route
+app.use((req,res,next)=>{
+    const exception = new Error(`Path not found`);
+    exception.statusCode = 404;
+    next(exception)
+})
+
+//customize express error handling middleware
+app.use((err,req,res,next)=>{
+    res.status(err.statusCode).send(err.message)
+})
 
 module.exports = app;
